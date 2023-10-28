32efb60e8edfba979c4953a5d8779e6d90cf2c4f
youthofmay123 <nhathuy39337343@gmail.com>
2023-10-28 18:19:14 +0700

    Add SanPham_BUS, SanPham_DAO, ISanPham, Edit SanPham, NhaXuatBan

---
+++ Nhom06_QuanLyWhimsiBooks/src/bus/SanPham_BUS.java
@@ -0,0 +1,42 @@
+package bus;
+
+import java.util.ArrayList;
+
+import dao.SanPham_DAO;
+import entities.SanPham;
+import interfaces.ISanPham;
+
+public class SanPham_BUS implements ISanPham{
+
+	SanPham_DAO sanPham_DAO = new SanPham_DAO();
+	@Override
+	public ArrayList<SanPham> getDanhSachSanPham(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void searchSanPham(String s, ArrayList<SanPham> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean addSanPham(SanPham sp) {
+		// TODO Auto-generated method stub
+		return sanPham_DAO.addSanPham(sp);
+	}
+
+	@Override
+	public boolean editSanPham(SanPham sp) {
+		// TODO Auto-generated method stub
+		return sanPham_DAO.editSanPham(sp);
+	}
+
+	@Override
+	public ArrayList<SanPham> getDanhSachSanPhamSapHet() {
+		// TODO Auto-generated method stub
+		return sanPham_DAO.getDanhSachSanPhamSapHet();
+	}
+
+}
