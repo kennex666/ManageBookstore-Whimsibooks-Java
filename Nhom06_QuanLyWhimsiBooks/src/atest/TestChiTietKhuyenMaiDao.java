39eb0e5bdf48995a80d209af89433afd13330263
EchhOpp <ngthluan285@gmail.com>
2024-04-12 20:09:43 +0700

    change addCTKM of CTKM

--- Nhom06_QuanLyWhimsiBooks/src/atest/TestChiTietKhuyenMaiDao.java
+++ Nhom06_QuanLyWhimsiBooks/src/atest/TestChiTietKhuyenMaiDao.java
@@ -12,11 +12,12 @@ public class TestChiTietKhuyenMaiDao {
 
 		ChiTietKhuyenMai_DAO chiTietKhuyenMai_DAO = new ChiTietKhuyenMai_DAO();
 //		System.out.println(chiTietKhuyenMai_DAO.getAllChiTietKhuyenMai());
-		System.out.println(chiTietKhuyenMai_DAO.getChiTietKhuyenMaiTheoMa("APPLY_PRODUCT_3"));
+//		System.out.println(chiTietKhuyenMai_DAO.getChiTietKhuyenMaiTheoMa("APPLY_PRODUCT_3"));
 //		System.out.println(chiTietKhuyenMai_DAO.getNgayTao("a"));
 //		System.out.println(chiTietKhuyenMai_DAO.addSanPhamKhuyenMai(new KhuyenMai("a"), new SanPham(2)));
 		
 		//addSanPhamKhuyenMaiKhiUpdate("a", 2);
-//		System.out.println(chiTietKhuyenMai_DAO.addSanPhamKhuyenMaiKhiUpdate("APPLY_PRODUCT_3", 1));
+		System.out.println(chiTietKhuyenMai_DAO.addSanPhamKhuyenMaiKhiUpdate("APPLY_PRODUCT_3", 7));
+		System.out.println(chiTietKhuyenMai_DAO.addSanPhamKhuyenMaiKhiUpdate("APPLY_PRODUCT_3", 8));
 	}
 }
--- Nhom06_QuanLyWhimsiBooks/src/dao/ChiTietKhuyenMai_DAO.java
+++ Nhom06_QuanLyWhimsiBooks/src/dao/ChiTietKhuyenMai_DAO.java
@@ -16,6 +16,7 @@ import entities.NhaCungCap;
 import entities.SanPham;
 import interfaces.IChiTietKhuyenMai;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class ChiTietKhuyenMai_DAO implements IChiTietKhuyenMai{
 	private Connection conn;
@@ -54,37 +55,31 @@ public class ChiTietKhuyenMai_DAO implements IChiTietKhuyenMai{
 	}
 	
 	public boolean addSanPhamKhuyenMai(KhuyenMai khuyenMai, SanPham sanPham) {
-		String codeKhuyenMai = khuyenMai.getCodeKhuyenMai();
-		int maSP = sanPham.getSanPhamID();
-		String insertCTTKM = "INSERT INTO ChiTietKhuyenMai (NgayTao, SanPhamID, CodeKhuyenMai) VALUES (?,?,?)";
+		EntityTransaction tr = em.getTransaction();
 		try {
-			Calendar calendar = Calendar.getInstance();
-			PreparedStatement preparedStatement1 = conn.prepareStatement(insertCTTKM);
-			preparedStatement1.setDate(1, new java.sql.Date(calendar.getTime().getTime()));
-			preparedStatement1.setInt(2, maSP);
-			preparedStatement1.setString(3, codeKhuyenMai);
-			preparedStatement1.executeUpdate();
+			tr.begin();
+			ChiTietKhuyenMai chiTietKhuyenMai = new ChiTietKhuyenMai(khuyenMai, sanPham, new Date(Calendar.getInstance().getTime().getTime()));
+			em.persist(chiTietKhuyenMai);
+			tr.commit();
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
 		}
-		return false;
 	}
 	
 	public boolean addSanPhamKhuyenMaiKhiUpdate(String makhuyenMai,int masanPham) {;
-		String insertCTTKM = "INSERT INTO ChiTietKhuyenMai (NgayTao, SanPhamID, CodeKhuyenMai) VALUES (?,?,?)";
-		try {
-			Calendar calendar = Calendar.getInstance();
-			PreparedStatement preparedStatement1 = conn.prepareStatement(insertCTTKM);
-			preparedStatement1.setDate(1, new java.sql.Date(calendar.getTime().getTime()));
-			preparedStatement1.setInt(2, masanPham);
-			preparedStatement1.setString(3, makhuyenMai);
-			preparedStatement1.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
+		EntityTransaction tr = em.getTransaction();
+        try {
+            tr.begin();
+            ChiTietKhuyenMai chiTietKhuyenMai = new ChiTietKhuyenMai(em.find(KhuyenMai.class, makhuyenMai), em.find(SanPham.class, masanPham), new Date(Calendar.getInstance().getTime().getTime()));
+            em.persist(chiTietKhuyenMai);
+            tr.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
 	}
 
 	public ChiTietKhuyenMai_DAO() {
