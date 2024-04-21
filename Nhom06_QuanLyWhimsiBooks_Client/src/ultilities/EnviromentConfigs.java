2592ce52224e154fb304ebaf554137af739e398c
Dương Thái Bảo <me@dtbao.io.vn>
2024-04-21 17:00:33 +0700

    ci: add enviroment

--- Nhom06_QuanLyWhimsiBooks_Client/src/ultilities/EnviromentConfigs.java
+++ Nhom06_QuanLyWhimsiBooks_Client/src/ultilities/EnviromentConfigs.java
@@ -4,6 +4,6 @@ public class EnviromentConfigs {
 	public static String STMP_EMAIL = "whimsibooks@1boxstudios.com";
 	public static String STMP_USERNAME = "";
 	public static String STMP_PASSWORD = "";
-	
+	//	public static String URL_RMI = "DESKTOP-802B4JP:2003";
 	public static String URL_RMI = "localhost:2003";
 }
